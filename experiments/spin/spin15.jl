8c319f9245ec4e2cd0cfacb6487471244372a70c
Thomas Propson <tcpropson@gmail.com>
2020-06-16 18:10:42 -0500

    progress on t1 modeling

---
+++ experiments/spin/spin15.jl
@@ -0,0 +1,304 @@
+"""
+spin15.jl - vanilla w/ T1
+"""
+
+using HDF5
+using LinearAlgebra
+using TrajectoryOptimization
+import Plots
+using Printf
+using StaticArrays
+
+# Construct paths.
+EXPERIMENT_META = "spin"
+EXPERIMENT_NAME = "spin13"
+WDIR = ENV["ROBUST_QOC_PATH"]
+SAVE_PATH = joinpath(WDIR, "out", EXPERIMENT_META, EXPERIMENT_NAME)
+
+# Plotting configuration.
+ENV["GKSwstype"] = "nul"
+Plots.gr()
+DPI = 300
+
+function generate_save_file_path(save_file_name, save_path)
+    # Ensure the path exists.
+    mkpath(save_path)
+
+    # Create a save file name based on the one given; ensure it will
+    # not conflict with others in the directory.
+    max_numeric_prefix = -1
+    for (_, _, files) in walkdir(save_path)
+        for file_name in files
+            if occursin("_$save_file_name.h5", file_name)
+                max_numeric_prefix = max(parse(Int, split(file_name, "_")[1]))
+            end
+        end
+    end
+
+    save_file_name = "_$save_file_name.h5"
+    save_file_name = @sprintf("%05d%s", max_numeric_prefix + 1, save_file_name)
+
+    return joinpath(save_path, save_file_name)
+end
+
+
+function plot_controls(controls_file_path, save_file_path,
+                       title=nothing)
+    # Grab and prep data.
+    (
+        controls,
+        evolution_time,
+        states,
+    ) = h5open(controls_file_path, "r+") do save_file
+        controls = read(save_file, "controls")
+        evolution_time = read(save_file, "evolution_time")
+        states = read(save_file, "states")
+        return (
+            controls,
+            evolution_time,
+            states
+        )
+    end
+    (control_eval_count, control_count) = size(controls)
+    control_eval_times = Array(range(0., stop=evolution_time, length=control_eval_count))
+    file_name = split(basename(controls_file_path), ".h5")[1]
+    if isnothing(title)
+        title = file_name
+    end
+
+    # Plot.
+    if false
+        fig = Plots.plot(control_eval_times, controls[:, 1], show=false, dpi=DPI)
+    else
+        fig = Plots.plot(control_eval_times, states[1:end-1, CONTROLS_IDX], show=false, dpi=DPI,
+                         label="controls", title=title)
+        Plots.xlabel!("Time (ns)")
+        Plots.ylabel!("Amplitude (GHz)")
+    end
+    Plots.savefig(fig, save_file_path)
+    return
+end
+
+
+# Define experimental constants.
+OMEGA = 2 * pi * 1.4e-2
+DOMEGA = OMEGA * 5e-2
+OMEGA_PLUS = OMEGA + DOMEGA
+OMEGA_MINUS = OMEGA - DOMEGA
+MAX_CONTROL_NORM_0 = 2 * pi * 3e-1
+# This is the state count used in the T1 calculations
+FLUXONIUM_STATE_COUNT = 8
+FLUXONIUM_LEVELS = range(start=0, stop=FLUXONIUM_STATE_COUNT,
+                         length=FLUXONIUM_STATE_COUNT)
+CREATE = diagm(1 => sqrt(FLUXONIUM_LEVELS))
+ANNIHILATE = diagm(-1 => sqrt(FLUXONIUM_LEVELS))
+# E / h
+EC = 0.479e9
+EL = 0.132e9
+EJ = 3.395e9
+# Q_CAP = 1 / 8e-6
+Q_CAP = 1.25e5
+T_CAP = 0.042
+HBAR_BY_KB = 7.63823e-12
+GAMMA_CAP_PREFACTOR = OMEGA^2 * coth(HBAR_BY_KB * OMEGA / (2 * T_CAP)) / (16 * pi * EC * Q_CAP)
+PHI_OSC = (8 * EC / EL)^(0.25)
+PHI_OP = PHI_OSC * 2^(-0.5) * (CREATE + ANNIHILATE)
+# Define the system.
+
+function t1(flux :: Float64)
+    gamma_cap = 0
+    
+end
+
+NEG_I = SA_F64[0   0  1  0 ;
+               0   0  0  1 ;
+               -1  0  0  0 ;
+               0  -1  0  0 ;]
+SIGMA_X = SA_F64[0   1   0   0;
+                 1   0   0   0;
+                 0   0   0   1;
+                 0   0   1   0]
+SIGMA_Z = SA_F64[1   0   0   0;
+                 0  -1   0   0;
+                 0   0   1   0;
+                 0   0   0  -1]
+H_S = SIGMA_Z / 2
+NEG_I_H_S = NEG_I * H_S
+OMEGA_NEG_I_H_S = OMEGA * NEG_I_H_S
+H_C1 = SIGMA_X / 2
+NEG_I_H_C1 = NEG_I * H_C1
+
+# Define the optimization.
+EVOLUTION_TIME = 40.
+COMPLEX_CONTROLS = false
+CONTROL_COUNT = 1
+DT = 1e-2
+N = Int(EVOLUTION_TIME / DT) + 1
+ITERATION_COUNT = Int(1e3)
+
+# Define the problem.
+INITIAL_STATE = SA[1., 0, 0, 0]
+STATE_SIZE, = size(INITIAL_STATE)
+INITIAL_ASTATE = [
+    INITIAL_STATE; # state
+    @SVector zeros(1); # int_control
+    @SVector zeros(1); # control
+    @SVector zeros(1); # dcontrol_dt
+]
+ASTATE_SIZE, = size(INITIAL_ASTATE)
+TARGET_STATE = SA[0, 1., 0, 0]
+TARGET_ASTATE = [
+    TARGET_STATE;
+    @SVector zeros(1); # int_control
+    @SVector zeros(1); # control
+    @SVector zeros(1); # dcontrol_dt
+]
+STATE_IDX = 1:STATE_SIZE
+INT_CONTROLS_IDX = 1 * STATE_SIZE + 1:1 * STATE_SIZE + CONTROL_COUNT
+CONTROLS_IDX = 1 * STATE_SIZE + CONTROL_COUNT + 1:1 * STATE_SIZE + 2 * CONTROL_COUNT
+DCONTROLS_DT_IDX = 1 * STATE_SIZE + 2 * CONTROL_COUNT + 1:1 * STATE_SIZE + 3 * CONTROL_COUNT
+
+
+# Generate initial controls.
+GRAB_CONTROLS = false
+INITIAL_CONTROLS = nothing
+if GRAB_CONTROLS
+    controls_file_path = joinpath(SAVE_PATH, "00000_spin12.h5")
+    INITIAL_CONTROLS = h5open(controls_file_path, "r") do save_file
+        controls = Array(save_file["controls"])
+        return [
+            SVector{CONTROL_COUNT}(controls[i]) for i = 1:N-1
+        ]
+    end
+else
+    # INIITAL_CONTROLS should be small if optimizing over derivatives.
+    INITIAL_CONTROLS = [
+        @SVector fill(1e-4, CONTROL_COUNT) for k = 1:N-1
+    ]
+end
+
+# Specify logging.
+VERBOSE = true
+SAVE = true
+
+struct Model <: AbstractModel
+    n :: Int
+    m :: Int
+end
+
+
+function Base.size(model::Model)
+    return model.n, model.m
+end
+
+
+function TrajectoryOptimization.dynamics(model::Model, astate, d2controls_dt2, time)
+    neg_i_control_hamiltonian = astate[CONTROLS_IDX][1] * NEG_I_H_C1
+    delta_state = (OMEGA_NEG_I_H_S + neg_i_control_hamiltonian) * astate[STATE_IDX]
+    delta_int_control = astate[CONTROLS_IDX]
+    delta_control = astate[DCONTROLS_DT_IDX]
+    delta_dcontrol_dt = d2controls_dt2
+    return [
+        delta_state;
+        delta_int_control;
+        delta_control;
+        delta_dcontrol_dt;
+    ]
+end
+
+
+function run_traj()
+    dt = DT
+    n = ASTATE_SIZE
+    m = CONTROL_COUNT
+    t0 = 0.
+    tf = EVOLUTION_TIME
+    x0 = INITIAL_ASTATE
+    xf = TARGET_ASTATE
+    # control amplitude constraint
+    x_max = [
+        @SVector fill(Inf, STATE_SIZE);
+        @SVector fill(Inf, 1);
+        @SVector fill(MAX_CONTROL_NORM_0, 1); # control
+        @SVector fill(Inf, 1);
+    ]
+    x_min = [
+        @SVector fill(-Inf, STATE_SIZE);
+        @SVector fill(-Inf, 1);
+        @SVector fill(-MAX_CONTROL_NORM_0, 1); # control
+        @SVector fill(-Inf, 1);
+    ]
+    # controls start and end at 0
+    x_max_boundary = [
+        @SVector fill(Inf, STATE_SIZE);
+        @SVector fill(Inf, 1);
+        @SVector fill(0, 1); # control
+        @SVector fill(Inf, 1);
+    ]
+    x_min_boundary = [
+        @SVector fill(-Inf, STATE_SIZE);
+        @SVector fill(-Inf, 1);
+        @SVector fill(0, 1); # control
+        @SVector fill(-Inf, 1);
+    ]
+
+    model = Model(n, m)
+    U0 = INITIAL_CONTROLS
+    X0 = [
+        @SVector fill(NaN, n) for k = 1:N
+    ]
+    Z = Traj(X0, U0, dt * ones(N))
+
+    Q = Diagonal([
+        @SVector fill(1e-1, STATE_SIZE);
+        @SVector fill(1e-1, 1); # int_control
+        @SVector fill(1e-1, 1); # control
+        @SVector fill(1e-1, 1); # dcontrol_dt
+    ])
+    Qf = Q * N
+    R = Diagonal(@SVector fill(1e-1, m))
+    obj = LQRObjective(Q, R, Qf, xf, N)
+
+    # must satisfy control amplitudes
+    control_bnd = BoundConstraint(n, m, x_max=x_max, x_min=x_min)
+    # must statisfy conrols start and stop at 0
+    control_bnd_boundary = BoundConstraint(n, m, x_max=x_max_boundary, x_min=x_min_boundary)
+    # must reach target state, must have zero net flux
+    target_astate_constraint = GoalConstraint(xf, [STATE_IDX;INT_CONTROLS_IDX])
+    
+    constraints = ConstraintSet(n, m, N)
+    add_constraint!(constraints, control_bnd, 2:N-2)
+    add_constraint!(constraints, control_bnd_boundary, 1:1)
+    add_constraint!(constraints, control_bnd_boundary, N-1:N-1)
+    add_constraint!(constraints, target_astate_constraint, N:N)
+    
+    prob = Problem{RK4}(model, obj, constraints, x0, xf, Z, N, t0, tf)
+    opts = SolverOptions(verbose=VERBOSE)
+    solver = ALTROSolver(prob, opts)
+    solve!(solver)
+
+    controls_raw = controls(solver)
+    controls_arr = permutedims(reduce(hcat, map(Array, controls_raw)), [2, 1])
+    states_raw = states(solver)
+    states_arr = permutedims(reduce(hcat, map(Array, states_raw)), [2, 1])
+    Q_raw = Array(Q)
+    Q_arr = [Q_raw[i, i] for i in 1:size(Q_raw)[1]]
+    Qf_raw = Array(Qf)
+    Qf_arr = [Qf_raw[i, i] for i in 1:size(Qf_raw)[1]]
+    R_raw = Array(R)
+    R_arr = [R_raw[i, i] for i in 1:size(R_raw)[1]]
+    
+    # Save
+    if SAVE
+        save_file_path = generate_save_file_path(EXPERIMENT_NAME, SAVE_PATH)
+        @printf("Saving this optimization to %s\n", save_file_path)
+        h5open(save_file_path, "cw") do save_file
+            write(save_file, "controls", controls_arr)
+            write(save_file, "evolution_time", tf)
+            write(save_file, "states", states_arr)
+            write(save_file, "Q", Q_arr)
+            write(save_file, "Qf", Qf_arr)
+            write(save_file, "R", R_arr)
+        end
+    end
+end
---
+++ experiments/spin/t1vsflux.py
@@ -0,0 +1,268 @@
+"""
+t1vsflux.py
+
+Authors: Daniel Weiss, Helin Zhang
+"""
+
+import itertools
+import os
+
+import scqubits as qubit
+import matplotlib
+matplotlib.rcParams['text.usetex'] = False
+matplotlib.rcParams.update({'font.size': 22})
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.linalg as la
+import scipy.constants as const
+
+from qutip import *
+from scqubits.utils.spectrum_utils import get_matrixelement_table, matrix_element
+from scqubits.utils.sweep_plotting import bare_spectrum, dressed_spectrum, difference_spectrum
+from scqubits import HilbertSpace, InteractionTerm, ParameterSweep
+
+EXPERIMENT_META = "spin"
+WDIR = os.environ["ROBUST_QOC_PATH"]
+OUT_PATH = os.path.join(WDIR, "out", EXPERIMENT_META)
+SAVE_FILE_PATH = os.path.join(OUT_PATH, "t1vsflux.png")
+
+def main():
+    # (1/(2pi hbar 10^9))
+    J_to_GHz = 1. / (2. * np.pi * const.hbar * (10**9))  
+    GHz_to_J = 1./J_to_GHz
+
+    resonator = qubit.Oscillator(
+        E_osc = 5.7286,
+        truncated_dim=5
+    )
+
+    fluxonium = qubit.Fluxonium(
+        EJ = 3.395,
+        EL = 0.132,
+        EC = 0.479,
+        flux = 0.5,
+        cutoff = 110,
+        truncated_dim = 60
+    )
+
+    hilbertspc = HilbertSpace([fluxonium, resonator])
+
+    adag = resonator.creation_operator()
+    a = resonator.annihilation_operator()
+
+    int_term = InteractionTerm(
+        g_strength = 0.076, 
+        subsys1 = fluxonium, 
+        op1 = fluxonium.n_operator(), 
+        subsys2 = resonator, 
+        op2 = a+adag
+    )
+
+    interaction_list = [int_term]
+    hilbertspc.interaction_list = interaction_list
+
+    dressed_hamiltonian = hilbertspc.get_hamiltonian()    
+
+    evals, evecs = dressed_hamiltonian.eigenstates(eigvals=10)
+    
+    # name of varying external parameter
+    param_name = 'flux'                          
+    # parameter values
+    param_vals = np.linspace(0.25, 0.5, 100)     
+    # list of HilbertSpace subsystems which are affected by parameter changes         
+    subsys_update_list = [fluxonium]             
+
+    # function that shows how Hilbert space components are updated
+    def update_hilbertspace(param_val):     
+        fluxonium.flux = param_val
+
+    sweep = ParameterSweep(
+        param_name=param_name,
+        param_vals=param_vals,
+        evals_count=50,
+        hilbertspace=hilbertspc,
+        subsys_update_list=subsys_update_list,
+        update_hilbertspace=update_hilbertspace
+    )
+
+    dressed_spectrum(sweep, xlim=(0.3,0.4),ylim=(0,7))
+
+    temp=0.05
+
+    def nthermal(omega):
+        return(1./(np.exp(omega*GHz_to_J/(const.k*temp))-1.))
+
+    def Pthermalprob(n, resonator):
+        return((1-np.exp(-resonator.E_osc*GHz_to_J/(const.k*temp)))
+               *np.exp(-n*resonator.E_osc*GHz_to_J/(const.k*temp)))
+
+    
+    def gamma_purcell_sweep_old(i, j, sweep, kappa, k, flux_val):
+        #i -> j
+        evals_dressed, evecs_dressed = (sweep.dressed_specdata.energy_table[k],
+                                        sweep.dressed_specdata.state_table[k])
+        fluxonium.flux = flux_val
+        evals_fluxonium, evecs_fluxonium = fluxonium.eigensys(evals_count=20)
+        gammap = 0.0
+        if (evals_fluxonium[i] > evals_fluxonium[j]):
+            #n1 ranges over initial states of the resonator
+            for n1 in range(2):
+                #n2 over final
+                for n2 in range(2):
+                    indexin1 = sweep.lookup.dressed_index((i,n1), param_index=k)
+                    indexjn2 = sweep.lookup.dressed_index((j,n2), param_index=k)
+                    gammap += (Pthermalprob(n1, resonator)
+                               * np.abs(matrix_element(
+                                   evecs_dressed[indexjn2],
+                                   sweep.hilbertspace.identity_wrap(
+                                       resonator.annihilation_operator(),
+                                       resonator), evecs_dressed[indexin1]))**2)
+            omegaij = evals_fluxonium[i]-evals_fluxonium[j]
+            return(10**9 * kappa * (nthermal(omegaij) + 1) * gammap)
+        else:
+            #n1 ranges over initial states of the resonator
+            for n1 in range(2):
+                #n2 over final
+                for n2 in range(2):
+                    indexin1 = sweep.lookup.dressed_index((i,n1), param_index=k)
+                    indexjn2 = sweep.lookup.dressed_index((j,n2), param_index=k)
+                    gammap += (Pthermalprob(n1, resonator)
+                                *np.abs(matrix_element(
+                                    evecs_dressed[indexjn2],
+                                    sweep.hilbertspace.identity_wrap(
+                                        resonator.creation_operator(),
+                                        resonator),evecs_dressed[indexin1]))**2)
+            omegaij = evals_fluxonium[j]-evals_fluxonium[i]
+            return(10**9 * kappa * (nthermal(omegaij)) * gammap)
+        #ENDDEF
+
+    
+    def gamma_purcell_sweep(i, j, sweep, kappa, k, flux_val):
+        #i -> j
+        evals_dressed, evecs_dressed = (sweep.dressed_specdata.energy_table[k],
+                                        sweep.dressed_specdata.state_table[k])
+        fluxonium.flux = flux_val
+        evals_fluxonium, evecs_fluxonium = fluxonium.eigensys(evals_count=20)
+        gammap = 0.0
+        if (evals_fluxonium[i] > evals_fluxonium[j]):
+            for n1 in range(2):
+                for n2 in range(2):
+                    indexin1 = sweep.lookup.dressed_index((i,n1), param_index=k)
+                    indexjn2 = sweep.lookup.dressed_index((j,n2), param_index=k)
+                    omegaij = np.abs(evals_dressed[indexin1]-evals_dressed[indexjn2])
+                    gammap += (Pthermalprob(n1, resonator) * (nthermal(omegaij) + 1)
+                               *np.abs(matrix_element(
+                                   evecs_dressed[indexjn2],
+                                   hilbertspc.identity_wrap(resonator.annihilation_operator(),
+                                                            resonator),evecs_dressed[indexin1]))**2)
+            return(10**9 * kappa * gammap)
+        else:
+            for n1 in range(2):
+                for n2 in range(2):
+                    indexin1 = sweep.lookup.dressed_index((i,n1), param_index=k)
+                    indexjn2 = sweep.lookup.dressed_index((j,n2), param_index=k)
+                    omegaij = np.abs(evals_dressed[indexin1]-evals_dressed[indexjn2])
+                    gammap += (Pthermalprob(n1, resonator) * (nthermal(omegaij))
+                                *np.abs(matrix_element(
+                                    evecs_dressed[indexjn2],
+                                    hilbertspc.identity_wrap(resonator.creation_operator(),
+                                                             resonator),evecs_dressed[indexin1]))**2)
+        return(10**9 * kappa * gammap)
+    #ENDDEF
+
+    
+    gamma_purcell_list = np.zeros(len(param_vals))
+    gamma_purcell_0up = np.zeros_like(gamma_purcell_list)
+    gamma_purcell_1up = np.zeros_like(gamma_purcell_list)
+    for k, flux_val in enumerate(param_vals):
+        gamma_purcell_list[k] = (gamma_purcell_sweep(0,1,sweep,0.00165, k, flux_val)
+                                 + gamma_purcell_sweep(1,0,sweep,0.00165, k, flux_val))
+        #j ranges over higher fluxonium levels
+        gamma_purcell_0up[k] = np.sum([gamma_purcell_sweep(0,j,sweep,0.00165,k,flux_val) for j in range(1,15)])
+        gamma_purcell_1up[k] = np.sum([gamma_purcell_sweep(1,j,sweep,0.00165,k,flux_val) for j in range(2,15)])
+    #ENDFOR
+
+    blist = np.linspace(0.25, 0.5, 100)
+    hbard2kb = const.hbar/const.k/2
+    spect = fluxonium.get_spectrum_vs_paramvals('flux', blist, evals_count=2, subtract_ground=True)
+    spect_table = spect.energy_table.T[1]*2*np.pi*1e9
+    wq = spect_table
+
+    levls = 8
+    philist = []
+    nlist = []
+    sinphilist = []
+    for ii in blist:
+        fluxonium.flux = ii
+        phi = abs((fluxonium.matrixelement_table('phi_operator', evals_count=levls)[0, 1]))**2
+        exit(0)
+        n =  abs((fluxonium.matrixelement_table('n_operator', evals_count=levls)[0, 1]))**2
+    #     sinphi =  abs((fluxonium.matrixelement_table('sinphi_operator', evals_count=levls)[0, 1]))**2
+        philist.append(phi)
+        nlist.append(n)
+    #     sinphilist.append(sinphi)
+    philist = np.array(philist)
+    nlist = np.array(nlist)
+    # sinphilist = np.array(sinphilist)
+
+    T_cap = 0.042
+    EC = 0.479e9*const.h
+    Gamma_cap = const.hbar * wq * wq / np.tanh(const.hbar*wq/const.k/T_cap/2) * philist / (8 * EC/(9e-6))
+
+    # fig, ax = plt.subplots(1,1, figsize=(10,5))
+    # ax.set_yscale('log')
+    # ax.plot(blist,1/Gamma_cap*1e6)
+    print(1/Gamma_cap[-1]*1e6)
+
+    T_ind = 0.042
+    EL = 0.132e9*const.h
+    Gamma_ind = 2*EL/const.hbar/np.tanh(const.hbar*wq/const.k/T_ind/2)*philist/(6e8)
+
+    # fig, ax = plt.subplots(1,1, figsize=(12,5))
+    # ax.set_yscale('log')
+    # ax.plot(blist,1/Gamma_ind*1e6)
+    print(1/Gamma_ind[-1]*1e6)
+
+    t1_cap = 1/Gamma_cap
+    t1_ind = 1/Gamma_ind
+    T1total = 1/(Gamma_cap + Gamma_ind + gamma_purcell_list + gamma_purcell_0up + gamma_purcell_1up)
+    T1purcell = 1./(gamma_purcell_list + gamma_purcell_0up + gamma_purcell_1up)
+    T1purcell01 = 1./gamma_purcell_list
+    T1purcell0up = 1./gamma_purcell_0up
+    T1purcell1up = 1./gamma_purcell_1up
+    #T1purcellperturb = 1./gamma_purcell_perturb_list
+    fig=plt.figure(figsize=(12,5))
+    plt.yscale("log")
+    plt.plot(param_vals, T1total, 'k-')
+    plt.plot(param_vals, T1purcell, 'b:')
+    # plt.plot(param_vals, T1purcell01, color="yellow")
+    # plt.plot(param_vals, T1purcell0up, color="red")
+    # plt.plot(param_vals, T1purcell1up, color="green")
+
+    # plt.plot(param_vals, t1_charge, color="orange")
+    plt.plot(param_vals, t1_cap, "r--")
+    plt.plot(param_vals, t1_ind, "g-.")
+    #plt.plot(param_vals, T1purcellperturb, color="purple")
+    errlist = 10**(-6)*np.array([78.034, 70.57, 20.71, 20.93, 12.92, 66.93, 137.06, 319.19, 
+                     194.84, 146.87, 170.584, 374.582, 322.533, 125.10, 105.987, 58.612, 22.295, 11.29])
+    t1_data = 10**(-6)*np.array([1597.923, 1627.93, 301.86, 269.03, 476.33, 1783.19, 2131.76, 2634.50, 
+                       4364.68, 2587.82, 1661.915, 1794.468, 2173.88, 1188.83, 
+                       1576.493, 965.183, 560.251, 310.88])
+    t1_flux = np.array([0.26, 0.28, 0.32, 0.34, 0.36, 0.38, 0.4, 0.42, 0.44, 0.46, 0.465, 
+                       0.47, 0.475, 0.48, 0.484, 0.488, 0.492, 0.5])
+    plt.errorbar(t1_flux,t1_data,errlist,fmt=' ',ecolor='k')
+    plt.plot(t1_flux,t1_data,'ko')
+    # plt.legend(("T1purcell","T1purcell01","T1purcell0up","T1purcell1up",
+    #             "T1total","t1_charge","t1_cap","t1_ind","data"), fontsize=12)
+    plt.legend(("T1total","T1purcell","t1_cap","t1_ind","data"), fontsize=12)
+    plt.ylim(10**(-4),2.2*10**(-2))
+    plt.xlim(0.245,0.505)
+    # plt.xlabel("flux")
+    # plt.ylabel("s")
+    # plt.show()
+    fig.savefig(SAVE_FILE_PATH)
+#ENDDEF
+
+
+if __name__ == "__main__":
+    main()
