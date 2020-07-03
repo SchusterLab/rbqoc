2a222e24dfe542682d90f342ed3f58c5f2616f6f
Thomas Propson <tcpropson@gmail.com>
2020-07-03 18:40:38 -0500

    cubic polynomial sampling complete

--- experiments/spin/spin15.jl
+++ experiments/spin/spin15.jl
@@ -6,6 +6,7 @@ using HDF5
 using LaTeXStrings
 using LinearAlgebra
 import Plots
+using Polynomials
 using Printf
 using StaticArrays
 using Statistics
@@ -22,8 +23,10 @@ SAVE_PATH = joinpath(WDIR, "out", EXPERIMENT_META, EXPERIMENT_NAME)
 ENV["GKSwstype"] = "nul"
 Plots.gr()
 DPI = 300
+MS = 2
+ALPHA = 0.2
 
-function generate_save_file_path(save_file_name, save_path)
+function generate_save_file_path(extension, save_file_name, save_path)
     # Ensure the path exists.
     mkpath(save_path)
 
@@ -32,13 +35,13 @@ function generate_save_file_path(save_file_name, save_path)
     max_numeric_prefix = -1
     for (_, _, files) in walkdir(save_path)
         for file_name in files
-            if occursin("_$save_file_name.h5", file_name)
+            if occursin("_$save_file_name.$(extension)", file_name)
                 max_numeric_prefix = max(parse(Int, split(file_name, "_")[1]))
             end
         end
     end
 
-    save_file_name = "_$save_file_name.h5"
+    save_file_name = "_$save_file_name.$(extension)"
     save_file_name = @sprintf("%05d%s", max_numeric_prefix + 1, save_file_name)
 
     return joinpath(save_path, save_file_name)
@@ -105,6 +108,88 @@ function t1_average(controls_file_path)
     t1s = map(get_t1_poly, controls / (2 * pi))
     t1_avg = mean(t1s)
     return t1_avg
+end
+
+
+"""
+sample_polynomial - Fit a polynomial to arbitrary data
+for a variable dt optimiation. Sample
+the polynomial at a given time step dt.
+"""
+function sample_polynomial(controls_file_path; plot=false)
+    # grab data
+    (
+        acontrols,
+        evolution_time,
+        astates,
+    ) = h5open(controls_file_path, "r+") do save_file
+        acontrols = read(save_file, "controls")
+        evolution_time = read(save_file, "evolution_time")
+        astates = read(save_file, "states")
+        return (
+            acontrols,
+            evolution_time,
+            astates
+        )
+    end
+    dts = acontrols[1:end, DT_IDX]
+    knot_count = size(dts)[1]
+    time_axis = [0; cumsum(dts, dims=1)[1:end-1]]
+    final_time_sample = sum(dts)
+    knot_count_sample = Int(floor(final_time_sample * DT_PREF_INV))
+    # the last control should be DT_PREF before final_time_sample
+    time_axis_sample = Array(0:1:knot_count_sample - 1) * DT_PREF
+
+    # fit a quadratic polynomial to each knot point, ends use their inner-neighbor's polynomial
+    # astates has one more knot point than acontrols
+    controls = astates[1:end - 1, CONTROLS_IDX[1]]
+    controls_polynomials = []
+    d2controls_dt2 = acontrols[1:end, D2CONTROLS_DT2_IDX[1]]
+    d2controls_dt2_polynomials = []
+    deg = 2
+    append!(controls_polynomials, fit(time_axis[1:3], controls[1:3], deg))
+    append!(d2controls_dt2_polynomials, fit(time_axis[1:3], d2controls_dt2[1:3], deg))
+    for i = 2:knot_count - 1
+        inds = i - 1:i + 1
+        ts = time_axis[inds]
+        append!(controls_polynomials, fit(ts, controls[inds], deg))
+        append!(d2controls_dt2_polynomials, fit(ts, d2controls_dt2[inds], deg))
+    end
+    append!(controls_polynomials, controls_polynomials[end])
+    append!(d2controls_dt2_polynomials, d2controls_dt2_polynomials[end])
+    
+    # sample the new time axis using the polynomials
+    controls_sample = zeros(knot_count_sample)
+    d2controls_dt2_sample = zeros(knot_count_sample)
+    time_axis_index = 1
+    for i = 1:knot_count_sample
+        now = time_axis_sample[i]
+        # advance the time axis to the nearest point
+        while ((time_axis_index != knot_count)
+               && (abs(time_axis[time_axis_index] - now)
+                   >= abs(time_axis[time_axis_index + 1] - now)))
+            time_axis_index = time_axis_index + 1
+        end
+        controls_sample[i] = controls_polynomials[time_axis_index](now)
+        d2controls_dt2_sample[i] = d2controls_dt2_polynomials[time_axis_index](now)
+        # println("tas[$(i)]: $(now), ta[$(time_axis_index)]: $(time_axis[time_axis_index]) "
+        #         * "sval: $(controls_sample[i]), val: $(controls[time_axis_index])")
+    end
+
+    # plot
+    if plot
+        plot_file_path = generate_save_file_path("png", EXPERIMENT_NAME, SAVE_PATH)
+        fig = Plots.plot(dpi=DPI)
+        Plots.scatter!(time_axis, controls, label="controls data", markersize=MS, alpha=ALPHA)
+        Plots.scatter!(time_axis_sample, controls_sample, label="controls fit", markersize=MS, alpha=ALPHA)
+        Plots.scatter!(time_axis, d2controls_dt2, label="d2_controls_dt2 data")
+        Plots.scatter!(time_axis_sample, d2controls_dt2_sample, label="d2_controls_dt2 fit")
+        Plots.xlabel!("Time (ns)")
+        Plots.ylabel!("Amplitude (2 pi GHz)")
+        Plots.savefig(fig, plot_file_path)
+        println("Plotted to $(plot_file_path)")
+    end
+    return (controls_sample, d2controls_dt2_sample)
 end
 
 
@@ -176,14 +261,16 @@ H_C1 = SIGMA_X / 2
 NEG_I_H_C1 = NEG_I * H_C1
 
 # Define the optimization.
-EVOLUTION_TIME = 17.86
+EVOLUTION_TIME = 20.0
 COMPLEX_CONTROLS = false
 CONTROL_COUNT = 1
 DT_INTEGRATOR = 1
-DT_MIN = 1e-3
-DT_INIT = 5e-3
-DT_INIT_INV = 2e2
+DT_MIN = 5e-3
+DT_INIT = 1e-2
+DT_INIT_INV = 1e2
 DT_MAX = 1e-2
+DT_PREF = 1e-2
+DT_PREF_INV = 1e2
 N = Int(EVOLUTION_TIME * DT_INIT_INV) + 1
 ITERATION_COUNT = Int(1e3)
 
@@ -200,8 +287,8 @@ INITIAL_ASTATE = [
     @SVector zeros(1); # int_gamma
 ]
 ASTATE_SIZE, = size(INITIAL_ASTATE)
-TARGET_STATE_0 = SA[1., 1, 0, 0] / sqrt(2)
-TARGET_STATE_1 = SA[-1., 1, 0, 0] / sqrt(2)
+TARGET_STATE_0 = SA[1., 0, -1, 0] / sqrt(2)
+TARGET_STATE_1 = SA[0., 1, 0, 1] / sqrt(2)
 TARGET_ASTATE = [
     TARGET_STATE_0;
     TARGET_STATE_1;
@@ -342,7 +429,7 @@ function run_traj()
     Qf = Q * N
     R = Diagonal(SVector{m}([
         fill(1e-1, CONTROL_COUNT); # d2control_dt2
-        0; # dt
+        1; # dt
     ])) 
     obj = LQRObjective(Q, R, Qf, xf, N)
 
@@ -352,6 +439,7 @@ function run_traj()
     control_bnd_boundary = BoundConstraint(n, m, x_max=x_max_boundary, x_min=x_min_boundary)
     # must satisfy dt constraint
     dt_bnd = BoundConstraint(n, m, u_max=u_max, u_min=u_min)
+    # dt_bnd = BoundConstraint(n, m, u_max=u_max)
     # must reach target state, must have zero net flux
     target_astate_constraint = GoalConstraint(xf, [STATE_0_IDX; STATE_1_IDX; INT_CONTROLS_IDX]);
     
@@ -360,14 +448,14 @@ function run_traj()
     add_constraint!(constraints, control_bnd_boundary, 1:1)
     add_constraint!(constraints, control_bnd_boundary, N-1:N-1)
     add_constraint!(constraints, dt_bnd, 1:N-1)
-    constraints.constraints[4].params.ϕ = 1e1
-    constraints.constraints[4].params.μ0 = 1e5
+    # constraints.constraints[4].params.ϕ = 1e4
+    # constraints.constraints[4].params.μ0 = 1e12
     add_constraint!(constraints, target_astate_constraint, N:N);
     
-    prob = Problem{RK3}(model, obj, constraints, x0, xf, Z, N, t0, tf)
+    prob = Problem{TrajectoryOptimization.RK4}(model, obj, constraints, x0, xf, Z, N, t0, tf)
     opts = SolverOptions(verbose=VERBOSE)
     solver = AugmentedLagrangianSolver(prob, opts)
-    solve!(solver)
+    TrajectoryOptimization.solve!(solver)
 
     controls_raw = controls(solver)
     controls_arr = permutedims(reduce(hcat, map(Array, controls_raw)), [2, 1])
@@ -382,7 +470,7 @@ function run_traj()
     
     # Save
     if SAVE
-        save_file_path = generate_save_file_path(EXPERIMENT_NAME, SAVE_PATH)
+        save_file_path = generate_save_file_path("h5", EXPERIMENT_NAME, SAVE_PATH)
         @printf("Saving this optimization to %s\n", save_file_path)
         h5open(save_file_path, "cw") do save_file
             write(save_file, "controls", controls_arr)
